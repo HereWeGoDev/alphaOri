688e9d6de7f025e9659c7e17245024f4d69c802d
Devadarsh <84261532+HereWeGoDev@users.noreply.github.com>
2023-06-23 22:22:32 +0530

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+const emailForm = document.getElementById("emailSender");
 
+emailForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+
+  try {
+    const response = await fetch("http://localhost:3001/emailSender", {
+      method: "POST",
+      body: JSON.stringify({ title: subject, message }),
+    });
+
+    if (response.ok) {
+      alert("Email sent successfully");
+    } else {
+      alert("Email not sent!");
+    }
+  } catch (error) {
+    alert("error occurred");
+    alert(error);
+    console.log(error);
+  }
+});
